33ef329e276613b4c944b87fd7b0aa5f11687f88
Yatin <yatendragupta90@gmail.com>
2021-06-02 14:04:55 +0530

    changes

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,10 +4,10 @@ import "../Assests/Css/Banner.css";
 function Banner() {
   return (
     <>
-      <div id="home" className="h-[calc(100vh-4.75rem)] w-full">
+      <div id="home" className="h-[calc(100vh-4.75rem)] w-full ">
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
       </div>
-      <div id="tool"  className="h-[calc(100vh-4.75rem)] w-full">
+      <div id="tool" className="h-[calc(100vh-4.75rem)] w-full">
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
       </div>
       <div className="h-[calc(100vh-4.75rem)] w-full">
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,44 +10,43 @@ export default function Navbar() {
   };
 
   return (
-    <>
-      {/* md:max-w-screen-xl */}
-      <nav className="px-2 py-1 mx-auto nav">
-        <h6 className="brandName flex">
-          <span>&lt;</span>Yatendra
-          <span>&nbsp;</span>
-          <span>&#47;</span>
-          <span>&gt;</span>
-        </h6>
+    // md:max-w-screen-xl
+    // <hr/>
+    <nav className="px-2 py-1 mx-auto nav ">
+      <h6 className="brandName flex">
+        <span>&lt;</span>Yatendra
+        <span>&nbsp;</span>
+        <span>&#47;</span>
+        <span>&gt;</span>
+      </h6>
 
-        <button className="flex md:hidden cursor-pointer" id="hamburger">
-          <img className={isActive ? "hidden  border-none" : "block"} onClick={toggleClass} src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png" alt="block" width="40" height="40" />
-          <img className={isActive ? "block  border-none" : "hidden"} onClick={toggleClass} src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png" alt="hidden" width="40" height="40" />
-        </button>
+      <button className="flex md:hidden cursor-pointer" id="hamburger">
+        <img className={isActive ? "hidden  border-none" : "block"} onClick={toggleClass} src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png" alt="block" width="40" height="40" />
+        <img className={isActive ? "block  border-none" : "hidden"} onClick={toggleClass} src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png" alt="hidden" width="40" height="40" />
+      </button>
 
-        <ul className="nav_headers px-5 toggle hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">
-          <li className="Nav_links">
-            <a href="home" className="block md:inline-block  px-3 py-3 md:border-none">
-              Home
-            </a>
-          </li>
-          <li className="Nav_links">
-            <a href="tool" className="block md:inline-block  px-3 py-3 md:border-none">
-              ToolBelt
-            </a>
-          </li>
-          <li className="Nav_links">
-            <a href="#" className="block md:inline-block  px-3 py-3 md:border-none">
-              Project
-            </a>
-          </li>
-          <li className="Nav_links">
-            <a href="#" className="block md:inline-block  px-3 py-3 md:border-none">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </>
+      <ul className="nav_headers px-5 toggle hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">
+        <li className="Nav_links">
+          <a href="home" className="block md:inline-block  px-3 py-3 md:border-none">
+            Home
+          </a>
+        </li>
+        <li className="Nav_links">
+          <a href="tool" className="block md:inline-block  px-3 py-3 md:border-none">
+            ToolBelt
+          </a>
+        </li>
+        <li className="Nav_links">
+          <a href="#" className="block md:inline-block  px-3 py-3 md:border-none">
+            Project
+          </a>
+        </li>
+        <li className="Nav_links">
+          <a href="#" className="block md:inline-block  px-3 py-3 md:border-none">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
